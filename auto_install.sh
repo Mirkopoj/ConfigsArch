e3afd753369ac2f98a7df3d859d3bd86baf897c7
Mirko <mirkopoj@hotmail.com>
2023-03-06 07:30:41 -0300

    auto install

---
+++ auto_install.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+paru -S xorg xorg-xinit feh picom brave-bin alacritty btop calcurse dunst fish neofetch nvim qtile ranger rofi zathura pavucontrol
+
+mv ~/.config/.xinitrc ~/.xinitrc
--- blender/3.4/scripts/addons/Nexus-Buddy-2-Blender-Scripts-16.30/Blender-2.7-Addons/rename_all_uvs.py
+++
@@ -1,6 +0,0 @@
-import bpy
-
-for obj in bpy.context.selected_objects :
-    if obj.type == 'MESH':
-        for uvmap in  obj.data.uv_layers :
-            uvmap.name = 'UVMap'
