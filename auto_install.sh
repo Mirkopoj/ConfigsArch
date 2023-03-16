5eeb5812eea8a64da6e04fa1851ebd3c7948d139
Mirkopoj <mirkopoj@hotmail.com>
2023-03-16 12:05:53 -0300

    auto install mas pulido

--- auto_install.sh
+++ auto_install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-paru -S xorg xorg-xinit feh picom brave-bin alacritty btop calcurse dunst fish neofetch nvim qtile ranger rofi zathura pavucontrol exa bat openssh
+paru -S xorg xorg-xinit feh picom brave-bin alacritty btop calcurse dunst fish neofetch nvim qtile ranger rofi zathura pavucontrol exa bat openssh vimb zathura-pdf-poppler dash qalculate-gtk redshift betterlockscreen python-pip zip unzip ueberzug
 
-mv ~/.config/.xinitrc ~/.xinitrc
+pip install psutils
+
+cp ~/.config/.xinitrc ~/.xinitrc
+sudo ln -fs /bin/sh /bin/dash
