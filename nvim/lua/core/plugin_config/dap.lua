ee9b281dcc6680ec0b7121664cfa4c1ed45206cb
Mirko <mirkopoj@hotmail.com>
2023-03-17 13:34:55 -0300

    arguments debbuging

--- nvim/lua/core/plugin_config/dap.lua
+++ nvim/lua/core/plugin_config/dap.lua
@@ -10,3 +10,11 @@ vim.keymap.set("n", "<F7>", ":lua require'dap'.step_into()<CR>")
 vim.keymap.set("n", "<F8>", ":lua require'dap'.step_out()<CR>")
 vim.keymap.set("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
 vim.keymap.set("n", "<leader>db", ":lua require'dapui'.toggle()<CR>")
+
+local dap = require('dap')
+dap.configurations.cpp[1]["args"] = function ()
+	local t = {}
+	local str = vim.fn.input('Arguments: ')
+	local _ = str:gsub("[^%s]+",function(c) table.insert(t,c) end)
+	return t
+end
